f460a87e47f2ffc2e812238bdd6d1ad9c5a7e6ac
djherbis <djherbis@gmail.com>
2015-07-18 15:45:11 -0700

    first draft

---
+++ bufit.go
@@ -0,0 +1,108 @@
+package bufit
+
+import (
+	"container/heap"
+	"io"
+	"sync"
+	"sync/atomic"
+)
+
+// Buffer is used to provide multiple readers with access to a shared buffer.
+// Readers may join/leave at any time, however a joining reader will only
+// see whats currently in the buffer onwards. Data is evicted from the buffer
+// once all active readers have read that section.
+type Buffer struct {
+	mu   sync.RWMutex
+	cond *sync.Cond
+	off  int64
+	rh   readerHeap
+	data []byte
+	life
+}
+
+type life struct {
+	state int32
+}
+
+func (l *life) alive() bool { return atomic.LoadInt32(&l.state) == 0 }
+func (l *life) kill()       { atomic.AddInt32(&l.state, 1) }
+
+func (b *Buffer) fetch(r *reader) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+
+	for len(b.data) == 0 && b.alive() && r.alive() {
+		b.cond.Wait()
+	}
+
+	if !r.alive() {
+		return
+	}
+
+	r.off += int64(r.chunk)
+	diff := r.off - b.off
+	r.data = b.data[diff:]
+	r.chunk = len(r.data)
+
+	heap.Fix(&b.rh, r.i)
+	b.shift()
+}
+
+func (b *Buffer) drop(r *reader) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	heap.Remove(&b.rh, r.i)
+	b.shift()
+}
+
+func (b *Buffer) shift() {
+	if b.rh.Len() == 0 {
+		return
+	}
+
+	if diff := b.rh.Peek().off - b.off; diff > 0 {
+		b.data = b.data[diff:]
+		b.off += diff
+	}
+}
+
+// NextReader returns a new ReadCloser for this shared buffer.
+// Read/Close are safe to call concurrently with the buffers Write/Close methods.
+// Read calls will block if the Buffer is not Closed and contains no data.
+func (b *Buffer) NextReader() io.ReadCloser {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	r := &reader{
+		buf:   b,
+		chunk: len(b.data),
+		off:   b.off,
+		data:  b.data,
+	}
+	heap.Push(&b.rh, r)
+	return r
+}
+
+// Write appends the given data to the buffer. All active readers will
+// see this write.
+func (b *Buffer) Write(p []byte) (int, error) {
+	b.mu.Lock()
+	defer b.cond.Broadcast()
+	defer b.mu.Unlock()
+	b.data = append(b.data, p...)
+	return len(p), nil
+}
+
+// Close marks the buffer as complete. Readers will return io.EOF instead of blocking
+// when they reach the end of the buffer.
+func (b *Buffer) Close() error {
+	defer b.cond.Broadcast()
+	b.kill()
+	return nil
+}
+
+// New creates and returns a new Buffer
+func New() *Buffer {
+	buf := Buffer{}
+	buf.cond = sync.NewCond(&buf.mu)
+	return &buf
+}
---
+++ heap.go
@@ -0,0 +1,73 @@
+package bufit
+
+import "io"
+
+type readerHeap []*reader
+
+func (h readerHeap) Len() int           { return len(h) }
+func (h readerHeap) Less(i, j int) bool { return h[i].off < h[j].off }
+func (h readerHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].i = i
+	h[j].i = j
+}
+
+func (h *readerHeap) Push(x interface{}) {
+	// Push and Pop use pointer receivers because they modify the slice's length,
+	// not just its contents.
+	r := x.(*reader)
+	r.i = len(*h)
+	*h = append(*h, r)
+}
+
+func (h *readerHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func (h readerHeap) Peek() *reader {
+	return h[0]
+}
+
+type reader struct {
+	buf   *Buffer
+	i     int
+	off   int64
+	chunk int
+	data  []byte
+	life
+}
+
+func (r *reader) Read(p []byte) (n int, err error) {
+	if len(r.data) == 0 {
+		r.buf.fetch(r)
+	}
+	n = copy(p, r.data)
+	if r.data = r.data[n:]; len(r.data) == 0 {
+		err = io.EOF
+	}
+	if err == io.EOF {
+		if !r.alive() {
+			return n, err
+		} else if r.buf.alive() {
+			err = nil
+		} else {
+			r.buf.fetch(r)
+			if len(r.data) > 0 {
+				err = nil
+			}
+		}
+	}
+	return n, err
+}
+
+// break calls to read.
+func (r *reader) Close() error {
+	r.kill()
+	r.buf.cond.Broadcast()
+	r.buf.drop(r)
+	return nil
+}
